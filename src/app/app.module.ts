71115d50cab0bb1dd7caafa8628fc0b8b39de4f3
German Alvarez <germanalvarez15@gmail.com>
2020-07-21 17:56:09 -0300

    ssl certs changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SceneComponent } from './ar-module/scene/scene.component';
+import { HomeComponent } from './webpage/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, SceneComponent],
+  declarations: [AppComponent, SceneComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
