beb2977d2ba9a7e6c6a24e9d0ce8b627b6373e37
German Alvarez <germanalvarez15@gmail.com>
2020-08-03 19:59:43 -0300

    Horizontal style

--- src/app/webpage/frame/frame.component.ts
+++ src/app/webpage/frame/frame.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-frame',
   templateUrl: './frame.component.html',
-  styleUrls: ['./frame.component.css']
+  styleUrls: ['./frame.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class FrameComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  createShadowRoot() {}
 }
